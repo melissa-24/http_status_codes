3113f4697832315a3639b7b38130858a0be979a2
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-07-11 20:09:51 -0400

    saving

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path')
 const app = express();
 const port = 3000;
 
+app.use(cors());
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
